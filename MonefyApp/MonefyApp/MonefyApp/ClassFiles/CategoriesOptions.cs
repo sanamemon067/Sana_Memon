d58ee57e60cdc06d4908e28f233140e0f001029b
sanamemon067 <82104067+sanamemon067@users.noreply.github.com>
2021-04-07 20:27:53 +0545

    Add files via upload
    Uploading Solution files

---
+++ MonefyApp/MonefyApp/MonefyApp/ClassFiles/CategoriesOptions.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Reporting;
+
+
+namespace MonefyApp.ClassFiles
+{
+	/// <summary>
+	/// Description of CategoriesOptions.
+	/// </summary>
+	public class CategoriesOptions
+	{
+		/// <summary>
+		/// ENUM to to help support method calls so that user does not have to type out what Prompt they are working with
+		/// </summary>
+		public enum IncomeCategoryOptions
+		{
+			Deposits,
+			Salary,
+			Savings,
+		}
+		
+		/// <summary>
+		/// ENUM to to help support method calls so that user does not have to type out what Prompt they are working with
+		/// </summary>
+		public enum ExpenseCategoryOptions
+		{
+			Bills,
+			Car,
+			Clothes,
+			Communications,
+			EatingOut,
+			Entertainment,
+			Food,
+			Gifts,
+			Health,
+			House,
+			Pets,
+			Sports,
+			Taxi,
+			Toiletry,
+			Transport
+			
+		}
+		
+		
+	}
+}
